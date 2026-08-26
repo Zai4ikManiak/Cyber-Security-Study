b253166a8994fca37ad4ef764114d22133dd6908
alex <alexandrupricop13@gmail.com>
2026-08-26 03:59:57 +0300

    modified:   Makefile 	modified:   Zabbix/zabbix_server.mk

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
+OS := $(shell . /etc/os-release && printf '%s' "$$ID")
+VERSION := $(shell . /etc/os-release && printf '%s' "$$VERSION_ID")
 
-OS := $(shell grep -ie '^name=' /etc/os-release | awk -F '"' '{print $$2}' | tr '[:upper:]' '[:lower:]')
-VERSION := $(shell grep -ie "^version=" /etc/os-release | awk -F '"' '{print$$2}' | egrep -o "^[0-9]+\.[0-9]+")
+PKG_MNG := $(shell \
+           if command -v apt-get >/dev/null; then echo apt; \
+           elif command -v dnf >/dev/null; then echo dnf; \
+           elif command -v yum >/dev/null; then echo yum; \
+           fi)
+
+PKG_MNGR := $(shell \
+            if command -v dpkg >/dev/null; then echo dpkg; \
+            elif command -v rpm >/dev/null; then echo rpm; \
+            fi)
 
 help:
         @printf "\n%s\n" "List of targets:"
--- Zabbix/zabbix_server.mk
+++ Zabbix/zabbix_server.mk
@@ -1,4 +1,16 @@
-
-zabbix_server_install:
+zabbix_server_install: zabbix_server_install_$(OS)
         @printf "%s\n\n" "Installing Zabbix server on $(OS)$(VERSION)"
+        @printf "%s\n" "Install Zabbix server, fornted, agent2"
+        @$(PKG_MNG) install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent2
+        @printf "%s\n" "Install Zabbix agent 2 plugins"
+        @$(PKG_MNG) install -y zabbix-agent2-plugin-mongodb zabbix-agent2-plugin-mssql zabbix-agent2-plugin-postgresql 
+
+zabbix_server_install_ubuntu:
+        @printf "\n%s\n" "1. Install Zabbix repository"
         @wget https://repo.zabbix.com/zabbix/7.0/$(OS)/pool/main/z/zabbix-release/zabbix-release_latest_7.0+$(OS)$(VERSION)_all.deb
+        @$(PKG_MNGR) -i zabbix-release_latest_7.0+$(OS)$(VERSION)_all.deb
+        @$(PKG_MNG) update -y
+
+zabbix_server_install_centos:
+        @printf "\n%s\n" "1. Install Zabbix repository"
+        @echo "In progress"
